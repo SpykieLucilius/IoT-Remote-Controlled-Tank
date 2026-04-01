3a0ea2c14bf0b87c518e90b3e7993c6d889cb9ae
SpykieLucilius <spykie.lucilius@pm.me>
2026-04-01 18:37:31 +0300

    Fix MACAdressFetch by using specific library

--- MACAdressFetch.ino
+++ MACAdressFetch.ino
@@ -1,7 +1,13 @@
-#include <WiFi.h>
-void setup(){
+#include <Arduino.h>
+#include <esp_mac.h> 
+
+void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_MODE_STA);
-  Serial.println(WiFi.macAddress());
+  delay(1000); 
+  
+  uint8_t mac[6];
+  esp_read_mac(mac, ESP_MAC_WIFI_STA);
+  Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
-void loop(){}
+
+void loop() {}
